6ecd339315c981e5da06cb6c7eb13615561f59c5
Alan Cheung <cheung@rdacorp.com>
2020-05-20 00:01:18 -0400

    Create route Active Projects.

--- DHC.Web/DHC.Web.Client/src/app/modules/app-routing.module.ts
+++ DHC.Web/DHC.Web.Client/src/app/modules/app-routing.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DoorsComponent } from '../components/doors/doors.component';
 import { IndexComponent } from '../components/index/index.component';
+import { ProjectComponent } from '../components/project/project.component';
 
 /**
  * Application routes available.
  */
 const routes: Routes = [
   { path: '', component: IndexComponent },
-  { path: 'doors', component: DoorsComponent}
+  { path: 'doors', component: DoorsComponent },
+  { path: 'projects', component: ProjectComponent },
 ];
 
 @NgModule({
