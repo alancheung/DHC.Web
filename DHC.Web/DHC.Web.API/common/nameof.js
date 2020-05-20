f5d4c2b93f8be46915869581e51a00700821ca9f
Alan Cheung <cheung@rdacorp.com>
2020-05-20 00:20:53 -0400

    Remove *.js files from tracking. Makes installation on Pi a little faster.

--- DHC.Web/DHC.Web.API/common/nameof.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.nameof = void 0;
-const nameof = (name) => name;
-exports.nameof = nameof;
-//# sourceMappingURL=nameof.js.map
