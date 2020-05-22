d4db9e32e84c1544d0e6e2b9bb8843411e9e1f29
Alan Cheung <cheung@rdacorp.com>
2020-05-22 16:12:03 -0400

    Add location of sensor to sensor reading.

--- DHC.Web/DHC.Web.API/SQLite/tables/SensorReading.ts
+++ DHC.Web/DHC.Web.API/SQLite/tables/SensorReading.ts
@@ -13,6 +13,9 @@ export class SensorReading extends DateTable {
     /** Hostname of the sensor */
     public SourceHostName: string;
 
+    /** Location of the sensor */
+    public Location: string;
+
     /** Type of sensor used, eg HiLetgo DHT22 */
     public SensorModel: string;
 
@@ -26,6 +29,7 @@ export class SensorReading extends DateTable {
         super(data);
         if (data) {
             this.SourceHostName = data.SourceHostName;
+            this.Location = data.Location;
             this.SensorModel = data.SensorModel;
             this.ReadingType = data.ReadingType;
             this.ReadingValue = data.ReadingValue;
@@ -37,6 +41,7 @@ export class SensorReading extends DateTable {
             ${nameof<SensorReading>("ID")} INTEGER PRIMARY KEY AUTOINCREMENT,
             ${nameof<SensorReading>("StartDate")} TEXT NOT NULL,
             ${nameof<SensorReading>("SourceHostName")} TEXT NOT NULL,
+            ${nameof<SensorReading>("Location")} TEXT NOT NULL,
             ${nameof<SensorReading>("SensorModel")} TEXT NOT NULL,
             ${nameof<SensorReading>("ReadingType")} TEXT NOT NULL,
             ${nameof<SensorReading>("ReadingValue")} REAL NOT NULL)`;
@@ -49,21 +54,28 @@ export class SensorReading extends DateTable {
             (
                 ${nameof<SensorReading>("StartDate")}, 
                 ${nameof<SensorReading>("SourceHostName")}, 
+                ${nameof<SensorReading>("Location")}, 
                 ${nameof<SensorReading>("SensorModel")}, 
                 ${nameof<SensorReading>("ReadingType")}, 
                 ${nameof<SensorReading>("ReadingValue")}
-            ) VALUES (?,?,?,?,?)`;
+            ) VALUES (?,?,?,?,?,?)`;
 
         return new SqlCommand(insert, [
             this.StartDate.toLocaleString(),
             this.SourceHostName,
+            this.Location,
             this.SensorModel,
             this.ReadingType.toString(),
             this.ReadingValue])
     }
 
     validate(): boolean {
-        return (!!this.StartDate && !!this.SourceHostName && !!this.SensorModel && !!this.ReadingType && (!!this.ReadingValue || this.ReadingValue == 0));
+        return (!!this.StartDate
+            && !!this.SourceHostName
+            && !!this.Location
+            && !!this.SensorModel
+            && !!this.ReadingType
+            && (!!this.ReadingValue || this.ReadingValue == 0));
     }
 
 }
