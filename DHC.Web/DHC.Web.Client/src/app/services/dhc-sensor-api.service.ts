e5fec1612d4a845e045f4827b625183597e46c42
Alan Cheung <cheung@rdacorp.com>
2020-06-02 00:00:54 -0400

    Add API to interact with Sensor API

---
+++ DHC.Web/DHC.Web.Client/src/app/services/dhc-sensor-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { DhcApiService } from './dhc-api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DhcSensorApiService extends DhcApiService {
+  /** Endpoint for Logs */
+  protected _serviceAddress: string;
+
+  constructor(private http: HttpClient) {
+    super(http);
+    this._serviceAddress = this._apiRoot + environment.sensorEndpoint;
+  }
+
+  public getSensorLocationsByNumberOfReadings(): Observable<any> {
+    return this._http.get<any>(this._serviceAddress);
+  }
+}
