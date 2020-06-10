04bbcf76d4f17a67eedb10202ab05115ec8900fd
Alan Cheung <cheung@rdacorp.com>
2020-06-10 13:43:32 -0400

    Show all known lights.

--- DHC.Web/DHC.Web.Client/src/app/components/lifx/lifx.component.ts
+++ DHC.Web/DHC.Web.Client/src/app/components/lifx/lifx.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { InformationLoaderComponent } from '../information-loader';
+import { LifxApiService } from '../../services/lifx-api.service';
+import { LightInfo } from '../../../../../DHC.Web.Common/models/models';
 
 @Component({
   selector: 'app-lifx',
   templateUrl: './lifx.component.html',
   styleUrls: ['./lifx.component.css']
 })
-export class LifxComponent implements OnInit {
+export class LifxComponent extends InformationLoaderComponent implements OnInit {
+  public KnownLights: LightInfo[];
 
-  constructor() { }
+  constructor(private api: LifxApiService) { super(); }
 
   ngOnInit(): void {
+    this.api.getDiscoveredLights()
+      .subscribe(lights => {
+        this.loading = false;
+        this.KnownLights = lights.sort((a, b) => a.LightName.localeCompare(b.LightName));
+      });
   }
 
+  discover(): void {
+    this.loading = true;
+
+    this.api.discover()
+      .subscribe(lights => {
+        this.loading = false;
+        this.KnownLights = lights.sort((a, b) => a.LightName.localeCompare(b.LightName));
+      });
+  }
+ 
 }
