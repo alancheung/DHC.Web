5a80a5db87dbf0c1347a6f54576ac91dd2241873
achippyland <cheung.alan94@gmail.com>
2021-05-30 23:27:49 -0400

    Create new Errors types.

---
+++ DHC.Web/DHC.Web.Common/models/definitions/Errors.ts
@@ -0,0 +1,17 @@
+export class ArgumentError extends Error {
+    constructor(msg: string) {
+        super(msg);
+    }
+}
+
+export class ArgumentMissingError extends ArgumentError {
+    constructor(objName: string, argName: string) {
+        super(`Missing ${objName} argument: ${argName}!`);
+    }
+}
+
+export class ArgumentOutOfRangeError extends ArgumentError {
+    constructor(objName: string, argName: string) {
+        super(`Out of range ${objName} argument: ${argName}!`);
+    }
+}
